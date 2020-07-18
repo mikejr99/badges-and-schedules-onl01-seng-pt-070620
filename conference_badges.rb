42d1a55b75eebe115040e4a078fcc8fedaccc766
Miguel Felix <mikeyfelix99@gmail.com>
2020-07-18 10:47:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,6 @@ def badge_maker(name)
   p "Hello, my name is #{name}."  
 end
 def batch_badge_creator(array)
-  array.each do |name|
-    badge_message = "Hello, my name is #{name}."
-  end
-  badge_message
+  array.collect do |name|
+    p "Hello, my name is #{name}."
 end
